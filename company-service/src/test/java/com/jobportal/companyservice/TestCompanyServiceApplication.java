319a19e0c2667cb689c60eb21d005d56d18df480
Marcos <marcoslopezmarinn@gmail.com>
2026-07-06 13:41:42 +0200

    initialize company service with Spring Boot and PostgreSQL configuration

---
+++ company-service/src/test/java/com/jobportal/companyservice/TestCompanyServiceApplication.java
@@ -0,0 +1,14 @@
+package com.jobportal.companyservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCompanyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication
+                .from(CompanyServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
+
+}
